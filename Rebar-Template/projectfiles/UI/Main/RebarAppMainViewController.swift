26049af514e73ee3758c090ba46db179fcfd0858
Harold Smith <h3smith@gmail.com>
2016-06-01 20:35:53 -0400

    Reworking some files to get the structure correct

--- Rebar-Template/projectfiles/UI/Main/RebarAppMainViewController.swift
+++ Rebar-Template/projectfiles/UI/Main/RebarAppMainViewController.swift
@@ -29,8 +29,6 @@ import RebarSupport
 @objc(RebarAppMainViewController)
 class RebarAppMainViewController : RebarCoreViewController, UITableViewDataSource, UITableViewDelegate {
 	
-	var refreshControl: UIRefreshControl?;
-	var hasLoaded: Bool = false;
 	var items: [StoredAppItem]? = [];
 	@IBOutlet var scheduleItemsTableView: UITableView!;
 	
@@ -48,13 +46,6 @@ class RebarAppMainViewController : RebarCoreViewController, UITableViewDataSourc
 	
 	override func viewWillAppear(animated: Bool) {
 		super.viewWillAppear(animated);
-		
-		if (!self.hasLoaded) {
-			self.hasLoaded = true;
-			delay(0.5) {
-				self.syncWithServer();
-			};
-		}
 		self.reload();
 	}
 	
@@ -66,13 +57,6 @@ class RebarAppMainViewController : RebarCoreViewController, UITableViewDataSourc
 		super.loadView();
 		
 		// Initialize the refresh control.
-		self.refreshControl = UIRefreshControl();
-		self.refreshControl!.backgroundColor = RebarAppController.getInstance()!.getConfiguration()!.defaultColor();
-		self.refreshControl!.tintColor = UIColor.whiteColor();
-		self.refreshControl?.addTarget(self, action: #selector(RebarAppMainViewController.manuallyRefreshData), forControlEvents: UIControlEvents.ValueChanged);
-		self.scheduleItemsTableView!.addSubview(self.refreshControl!);
-		
-		//
 		self.scheduleItemsTableView.registerNib(UINib(nibName: "ScheduleMainTableViewCell", bundle: NSBundle(forClass: self.dynamicType)), forCellReuseIdentifier: "ScheduleMainTableViewCell");
 		
 		self.title = "Rebar App";
@@ -93,13 +77,6 @@ extension RebarAppMainViewController {
 	/// Call the server
 	func syncWithServer() {
 		
-	}
-	
-	func manuallyRefreshData() {
-		// Ask the handler to sync...
-		delay(0.25) {
-			self.syncWithServer();
-		};
 	}
 }
 
